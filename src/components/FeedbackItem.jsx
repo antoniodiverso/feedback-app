f8c3a662cd33dc5b99b907c75c3621687e4c9b48
antoniodiverso <77039172+antoniodiverso@users.noreply.github.com>
2022-02-03 16:05:57 -0300

    Added a Card component and a Delete button
    A Card component with It's individual styling and a condicional styling through reverse.
    Delete buttton through drilling props from feedbackItem to feedbackList and the to App.js using the function handleDelete.

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,17 +1,21 @@
+import { FaTimes } from "react-icons/fa";
+import Card from "./shared/Card";
+import React from "react";
 
-import React from 'react'
-
-
-function FeedbackItem({item}) {
-
-
-    
+function FeedbackItem({ item, handleDelete }) {
   return (
-    <div className='card'>
-        <div className="num-display">{item.rating}</div>
-        <div className="text-display">{item.text}</div>
-    </div>
-  )
+    <Card>
+      <div className="num-display">{item.rating}</div>
+      <button onClick={()=> handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
+      <div className="text-display">{item.text}</div>
+    </Card>
+  );
 }
 
-export default FeedbackItem
+Card.defaultProps = {
+  reverse: false,
+};
+
+export default FeedbackItem;
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,18 +1,21 @@
-import FeedbackItem from "./FeedbackItem"
-import React from 'react'
+import FeedbackItem from "./FeedbackItem";
+import React from "react";
 
-function FeedbackList({feedback}) {
-    if (!feedback || feedback.lenght === 0){
-        return <p>No feedback yet :( </p>
-    }
-    return (
-   <div className="feedback-list">
-       {feedback.map((item)=>(
-           <FeedbackItem key={item.id} item={item}/>
-
-       ))}
-   </div>
-  )
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.lenght === 0) {
+    return <p>No feedback yet :( </p>;
+  }
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem
+          key={item.id}
+          item={item}
+          handleDelete={handleDelete}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default FeedbackList
+export default FeedbackList;
