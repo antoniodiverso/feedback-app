e786eb8199cc15be5982606a88baca95e2bb11ab
antoniodiverso <77039172+antoniodiverso@users.noreply.github.com>
2022-02-17 17:29:38 -0300

    NavLinks, useParams, Navigate and Nested Routes
    Lesson 28, 29 and 30

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutIconLink from "./components/AboutIconLink";
 import FeedbackData from "./data/FeedbackData";
 import Card from "./components/shared/Card";
 import AboutPage from "./pages/About";
+import {FeedbackProvider} from './context/FeedbackContext'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -24,6 +26,7 @@ function App() {
   };
 
   return (
+    <FeedbackProvider>
     <Router>
       <Header />
 
@@ -46,8 +49,10 @@ function App() {
 
           <Route path="/about" element={<AboutPage />} />
         </Routes>
+        <AboutIconLink />
       </div>
     </Router>
+    </FeedbackProvider>
   );
 }
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import Card from '../components/shared/Card'
+import React from "react";
+import Card from "../components/shared/Card";
+import { Link } from "react-router-dom";
 
 function About() {
   return (
     <Card>
       <div className="about">
-          <h1>About this proyect</h1>
-          <p>This is a React app to leave feedback for a product or a service</p>
-          <p>Version 1.0.0</p>
+        <h1>About this proyect</h1>
+        <p>This is a React app to leave feedback for a product or a service</p>
+        <p>Version 1.0.0</p>
+        <p>
+          <Link to="/">Return to Home</Link>
+        </p>
       </div>
     </Card>
-  )
+  );
 }
 
-export default About
+export default About;
